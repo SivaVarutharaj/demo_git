5abfa1fb934bcaa156f4eeb67fbe87cd5d9e9f5e
SivaVarutharaj <gorgevikki@gmail.com>
2026-03-07 11:13:09 +0530

    Delete Darling_Retail_Ecommerce_Project directory

--- Darling_Retail_Ecommerce_Project/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is Running on ${process.env.PORT}`);
-});
--- Darling_Retail_Ecommerce_Project/src/db/schemas/categoriesSchema.js
+++
@@ -1,9 +0,0 @@
-const { pgTable, serial, timestamp, varchar } = require("drizzle-orm/pg-core");
-
-const categories = pgTable("categories", {
-  id: serial("id").primaryKey(),
-  name: varchar("category_name", { length: 50 }).notNull(),
-  created_at: timestamp("created_at").defaultNow(),
-});
-
-module.exports = { categories };
